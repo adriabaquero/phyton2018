bb18899dfe027128cba70828e7ef5715ee1c66f2
adriabaquero <36151499+adriabaquero@users.noreply.github.com>
2018-02-08 21:42:51 +0100

    Add files via upload

---
+++ ejercicio-swap.py
@@ -0,0 +1,13 @@
+#coding:utf8
+#Adrià Baquero
+#08/02/2018
+
+
+mano_dcha="movil"
+mano_izqda="bocadillo"
+mano_extra=mano_dcha
+mano_dcha=mano_izqda
+mano_izqda=mano_extra
+
+print mano_dcha
+print mano_izqda
