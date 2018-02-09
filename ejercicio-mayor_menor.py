7c9880cd4b796c0b4dfce158515fb4cc69c74b2b
adriabaquero <36151499+adriabaquero@users.noreply.github.com>
2018-02-09 12:57:33 +0100

    Add files via upload

---
+++ ejercicio-mayor_menor.py
@@ -0,0 +1,11 @@
+#coding:utf8
+#Adrià Baquero
+#09/02/2018
+
+valor1=input ("Introduce el valor 1: ")
+valor2=input ("Introduce el valor 2: ")
+valor3=input ("Introduce el valor 3: ")
+
+if (valor1=valor2) and (valor3!=valor1):
+	print "Los 3 son iguales"
+else 
