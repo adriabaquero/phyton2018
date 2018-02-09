081df9c65dff2c318cfa6c46f33d6354c07789e7
adriabaquero <36151499+adriabaquero@users.noreply.github.com>
2018-02-09 12:57:23 +0100

    Add files via upload

---
+++ ejercicio-mayor.py
@@ -0,0 +1,13 @@
+#coding:utf8
+
+valor1=input ("Introduzca el primer valor: ")
+valor2=input ("Introduzca un segudno valor: ")
+
+if(valor1 > valor2):
+	print "El valor1 es mayor que el valor 2"
+else:
+	if(valor2 > valor1):
+		print "El valor 2 es mayor que el valor 1"
+	else:	
+		print "Son iguales"
+	
